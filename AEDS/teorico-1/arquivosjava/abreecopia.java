40cf49e44c661caadacbeaf92c8afe92fe542c2e
Fernando Araújo <97913564+lntra@users.noreply.github.com>
2022-08-15 20:39:30 -0300

    atividade arquivo 1

--- AEDS/teorico-1/arquivosjava/abreecopia.java
+++ AEDS/teorico-1/arquivosjava/abreecopia.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class abreecopia {
     public static void main(String[] args) {
-        Scanner leitura = new Scanner(arquivo);
+        Scanner leitura = new Scanner(System.in);
         Arq.openWrite("Entrada1.txt");
         System.out.println("Digite a frase desejada: ");
         String x = leitura.nextLine();
+        leitura.close();
         Arq.println(x);
         Arq.close();
         Arq.openWrite("Copia1.txt");
