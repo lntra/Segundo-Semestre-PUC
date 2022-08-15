7a86ad4f3fa3320062d31ce82d9478c53994f56e
Fernando Araújo <97913564+lntra@users.noreply.github.com>
2022-08-15 16:10:25 -0300

    arquivos de comandos

--- AEDS/teorico-1/arquivosjava/abreecopia.java
+++ AEDS/teorico-1/arquivosjava/abreecopia.java
@@ -1,3 +1,17 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class abreecopia {
-    
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(arquivo);
+        Arq.openWrite("Entrada1.txt");
+        System.out.println("Digite a frase desejada: ");
+        String x = leitura.nextLine();
+        Arq.println(x);
+        Arq.close();
+        Arq.openWrite("Copia1.txt");
+        Arq.println(x);
+        Arq.close();
+    }
 }
