60d9e153b25be0db7edf7dea3c2cde77d470aa08
Fernando Araújo <97913564+lntra@users.noreply.github.com>
2022-08-16 08:52:09 -0300

    atividade

---
+++ AEDS/teorico-1/arquivosjava/Leituradenome3.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.ZoneId;
+import java.util.Scanner;
+import java.util.zip.ZipOutputStream;
+
+public class Leituradenome3 {
+    public static void main(String[] args) {
+        File nomeArquivo = new File("meunome5.txt");
+        String nomeDoArquivo = nomeArquivo.getName();
+        try {
+        FileWriter arquivoRecebe = new FileWriter("recebenome5.txt");
+        arquivoRecebe.write(nomeDoArquivo);
+        arquivoRecebe.close();
+        } catch (IOException e) {
+            System.out.println("Erro");
+            e.printStackTrace();
+        }
+
+    }
+}
