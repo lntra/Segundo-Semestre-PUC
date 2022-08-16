d0f821f56f5cdfe1cfaaaedfa3556c926c21cfb9
Fernando Araújo <97913564+lntra@users.noreply.github.com>
2022-08-16 10:39:14 -0300

    tentativa

---
+++ AEDS/teorico-1/arquivosjava/Conteudo3.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.time.ZoneId;
+import java.util.RandomAccess;
+import java.util.Scanner;
+import java.util.zip.ZipOutputStream;
+
+public class Conteudo3 {
+    public static void main(String[] args) throws Exception{
+        String x = "";
+        RandomAccessFile raf = new RandomAccessFile("arquivoDeLeitura6.txt", "r") ;
+        for(int i = 32; i > 0; i--){
+            raf.seek(i);
+             x += raf.readChar();
+             System.out.println(x);
+
+        }
+        raf.close();
+        try {
+            FileWriter arquivoRecebe = new FileWriter("recebenome8.txt");
+            arquivoRecebe.write(x);
+            arquivoRecebe.close();
+        } catch (IOException e) {
+            System.out.println("Erro");
+            e.printStackTrace();
+        }
+
+    }
+}
