d5b6744eb713d84f407bbcb394e6e942c2724c53
Fernando Araújo <97913564+lntra@users.noreply.github.com>
2022-08-23 19:34:19 -0300

    arquivo . java

---
+++ TIAW_II/Exercicio 1/EclipseEgit/SomarDoisNumeros.java
@@ -0,0 +1,17 @@
+package EclipseEgit;
+
+import java.util.*;
+
+class SomarDoisNumeros{
+	public static void main (String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int num1, num2, soma;
+		System.out.println("Digite um numero");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro numero"); 
+		num2 = sc.nextInt();
+		soma =  num1 +  num2;
+		System.out.println("Soma:" + soma);          
+	}
+
+}
