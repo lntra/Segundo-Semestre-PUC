6f900a748d25d90b8260d3cac89fddab3d0b15bf
Fernando Araújo <97913564+lntra@users.noreply.github.com>
2022-08-26 00:53:17 -0300

    alteracao dao

--- TIAW_II/BD01JavaPostgreSQL/src/main/java/dao/DAO.java
+++ TIAW_II/BD01JavaPostgreSQL/src/main/java/dao/DAO.java
@@ -6,17 +6,17 @@ import java.math.*;
 
 public class DAO {
 	protected Connection conexao;
-	
+
 	public DAO() {
 		conexao = null;
 	}
-	
+
 	public boolean conectar() {
-		String driverName = "org.postgresql.Driver";                    
+		String driverName = "org.postgresql.Driver";
 		String serverName = "localhost";
 		String mydatabase = "postgres";
 		int porta = 5432;
-		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
+		String url = "jdbc:postgresql://" + serverName + ":" + porta + "/" + mydatabase;
 		String username = "admin";
 		String password = "admin";
 		boolean status = false;
@@ -26,7 +26,7 @@ public class DAO {
 			conexao = DriverManager.getConnection(url, username, password);
 			status = (conexao == null);
 			System.out.println("Conexão efetuada com o postgres!");
-		} catch (ClassNotFoundException e) { 
+		} catch (ClassNotFoundException e) {
 			System.err.println("Conexão NÃO efetuada com o postgres -- Driver não encontrado -- " + e.getMessage());
 		} catch (SQLException e) {
 			System.err.println("Conexão NÃO efetuada com o postgres -- " + e.getMessage());
@@ -34,10 +34,10 @@ public class DAO {
 
 		return status;
 	}
-	
+
 	public boolean close() {
 		boolean status = false;
-		
+
 		try {
 			conexao.close();
 			status = true;
@@ -46,11 +46,10 @@ public class DAO {
 		}
 		return status;
 	}
-	
-	
+
 	public static String toMD5(String senha) throws Exception {
-		MessageDigest m=MessageDigest.getInstance("MD5");
-		m.update(senha.getBytes(),0, senha.length());
-		return new BigInteger(1,m.digest()).toString(16);
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		m.update(senha.getBytes(), 0, senha.length());
+		return new BigInteger(1, m.digest()).toString(16);
 	}
 }
